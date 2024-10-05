4260c8c6155c4305f0f6190cdce77344a8dfabc1
asaenzg <asaenzg95@gmail.com>
2024-10-04 18:51:45 -0700

    pls check code for Q2

--- OneDrive/Escritorio/ASU/Industrial Organization/HW1/HW1.py
+++ OneDrive/Escritorio/ASU/Industrial Organization/HW1/HW1.py
@@ -11,6 +11,9 @@ import pandas as pd
 from linearmodels.panel import PanelOLS
 from sklearn.preprocessing import LabelEncoder
 import matplotlib.pyplot as plt
+from numba import jit
+import scipy as sp
+from scipy.optimize import minimize
 
 # Load data
 data_agents = pd.read_csv("Data/agent_data.csv")
@@ -63,6 +66,10 @@ m_ols = PanelOLS(dependent = data_products["rel_shares"],
 # m_ols = PanelOLS.from_formula("shares ~ constant + sugar + prices", data = data_products) # quivalent syntax
 
 m_ols.fit()
+print("================================================================================")
+print("                                 Simple OLS                                     ")
+print("================================================================================")
+print(m_ols.fit())
 
 ##############################################################################
 ######### Part 1.b
@@ -103,6 +110,10 @@ m_stg2 = PanelOLS(dependent = data_products["rel_shares"],
                     time_effects = False)
 
 m_stg2.fit()
+print("================================================================================")
+print("                                   2SLS OLS                                     ")
+print("================================================================================")
+print(m_stg2.fit())
 
 betas = m_stg2.fit().params
 
@@ -121,3 +132,110 @@ plt.title("Prices vs estimated elasticities in market C01Q1")
 plt.xlabel("Prices")
 plt.ylabel("Estimated elasticities")
 plt.show()
+
+
+##############################################################################
+###### Problem 2
+##############################################################################
+######### Part 2.a
+##############################################################################
+
+# Load data
+data_agents = pd.read_csv("Data/agent_data.csv")
+data_products = pd.read_csv("Data/product_data.csv")
+
+
+n_markets = data_products["market_ids"].unique().size
+n_products = data_products["product_ids"].unique().size
+n_agents = int(1/data_agents.iloc[0, 2])                                        # since equally weighted and all markets have same number of consumers
+
+np.random.seed(42)
+#Random coefficients
+theta = np.random.normal(loc = 0, 
+                               scale = 1, 
+                               size = 3)                                        # price & sugar
+sigma = np.random.normal(loc = 0,
+                               scale = 1,
+                               size = 3)                                        # income effect & taste_shock
+
+#U = delta + random_coef*charac
+def utility(delta, product_data, agent_data, random_coef):
+    price, sugar = product_data["prices"], product_data["sugar"]
+    income, taste_shock = agent_data["income"], agent_data["nodes0"]
+    
+    alpha = random_coef[3] + random_coef[4]*income + random_coef[5]*taste_shock
+    beta_0 = random_coef[0] + random_coef[1]*income
+    beta_sugar = random_coef[2] 
+    return delta + beta_0 + beta_sugar*sugar + alpha*price
+
+#Market shares calculation
+@jit
+def est_market_shares(delta, products, demographics, random_coef):
+    market_ids = data_products["market_ids"].unique()
+    mkt_shares = []
+    
+    for t in market_ids:
+        market_products = products[products["market_ids"] == t]
+        market_consumers = demographics[demographics["market_ids"] == t]
+        
+        #Utility for each consumer in market t
+        utilities = []
+        for i, product_data in market_products.iterrows():
+            product_util = []
+            for _, consumer_data in market_consumers.iterrows():
+                product_util.append(utility(delta, product_data, consumer_data, random_coef))
+            utilities.append(product_util)
+            
+        #From utilities to probabilities
+        utilities = np.array(utilities)
+        exp_utilities = np.exp(utilities)
+        mkt_probs = exp_utilities/np.sum(exp_utilities, axis = 0)
+        
+        #Avg Mkt Share
+        avg_mkt_share = np.mean(mkt_probs, axis = 1)
+        mkt_shares.append(avg_mkt_share)
+        
+    return np.concatenate(mkt_shares)
+    
+#Objective function: GMM to minimize observed and sim shares
+def gmm_obj(delta, products, demographics, observed_shares, random_coef):
+    predicted_shares = est_market_shares(delta, products, demographics, random_coef)
+    moment_error = observed_shares - predicted_shares
+    return np.sum(moment_error**2)                                              # we want to minimize square error
+
+#Estimate delta by minimizing the square error
+
+# observed_shares = data_products.loc[data_products["market_ids"]=="C01Q1", "shares"].values
+    
+observed_shares = data_products["shares"].values
+random_coef = np.concatenate([theta, sigma])
+
+delta_init = np.zeros(n_markets)
+result = minimize(gmm_obj, 
+                  delta_init, 
+                  args = (data_products, data_agents, observed_shares, random_coef), 
+                  method = "BFGS") 
+
+delta_est = result.x
+
+#Contraction mapping
+@jit
+def contraction_mapping(delta, products, demographics, observed_shares, random_coef, tolerance = 10e-15, max_iterations = 1000):
+    iter = 0
+    while iter < max_iterations:
+        predicted_shares = est_market_shares(delta, products, demographics, random_coef)
+        delta_new = delta + np.log(observed_shares / predicted_shares)
+        if np.max(np.abs(delta_new - delta)) < tolerance:
+            break
+        delta = delta_new
+        iter += 1
+    return delta
+
+#Apply contraction mapping
+delta_sol = contraction_mapping(delta_init, data_products, data_agents, observed_shares, random_coef)
+
+
+
+                                
+                                     
+                                
